cbf879d41af2f9a31e533cdf8c8053936254f3aa
lxm <909055383@qq.com>
2018-03-13 13:16:36 +0800

    包的使用

---
+++ linear regression/linear_package.py
@@ -0,0 +1,17 @@
+
+from sklearn import linear_model
+
+clf = linear_model.LinearRegression()
+
+#设置训练集
+X = [[0, 0], [1, 1], [2, 2]]   #两个自变量
+y = [0, 1, 2]
+#训练模型
+clf.fit(X, y)
+
+#预测
+print(clf.coef_)   #回归系数
+print(clf.intercept_)   #截距
+
+# print(y_pred)
+# print(y_true)
